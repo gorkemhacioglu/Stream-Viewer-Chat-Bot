1fcecd62a41e498eea6c8ea36de583819fd7954b
SCOOP-LAPTOP\gorkem <gorkemhacioglu@hotmail.com>
2021-01-18 19:57:17 +0300

    Add project files.

---
+++ TwitchBot/Program.cs
@@ -0,0 +1,87 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions.Internal;
+using OpenQA.Selenium.Opera;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwitchBot
+{
+    class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            //string[] proxyList = { "162.144.48.236:3838", "162.144.50.197:3838", "69.241.4.90:80", "59.29.245.151:3128" };
+            //while (i < proxyList.Length)
+            //{
+            //    Thread thr = new Thread(req);
+            //    thr.Start(proxyList[i]);
+            //    i++;
+
+            //};
+
+            string line = string.Empty;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Source\TwitchBot\TwitchBot\bin\Debug\netcoreapp3.1\proxylist.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                Thread thr = new Thread(req);
+                thr.Start(line);
+            }
+
+            file.Close();
+            Console.WriteLine("Waiting for end");
+            Console.ReadLine();
+
+        }
+
+        [Obsolete]
+        private static void req(object obj)
+        {
+            try
+            {
+                var proxy = new Proxy();
+                proxy.HttpProxy = obj.ToString();
+                proxy.SslProxy = obj.ToString();
+                //var opera_options = new OperaOptions();
+                //opera_options.Proxy = proxy;
+                //opera_options.AddArgument("--enable-blink-features=ShadowDOMV0");
+                //var driver = new OperaDriver(opera_options);
+                var chrome_options = new ChromeOptions();
+                chrome_options.Proxy = proxy;
+                chrome_options.AcceptInsecureCertificates = true;
+                //chrome_options.AddArgument("--proxy-server=" + proxyList[i]);
+                var driver = new ChromeDriver(chrome_options);
+
+                //driver.Url = "https://whatismyipaddress.com/";
+                driver.Url = "http://twitch.tv/klopsik733";
+                driver.Navigate();
+
+                while (true)
+                {
+                    var item = driver.FindElementByClassName("simplebar-scrollbar");
+
+                    if (item != null)
+                    {
+                        Random r = new Random();
+                        int rInt = r.Next(0, 5000);
+                        item.SendKeys(Keys.Down);
+                        Thread.Sleep(rInt);
+                        item.SendKeys(Keys.Up);
+                        Thread.Sleep(rInt);
+                        Console.WriteLine("GORKEM");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex);
+            }
+            return;
+        }
+    }
+}
